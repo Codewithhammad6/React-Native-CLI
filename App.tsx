6bd3059aee30e5b0d4557abe4c448573ffd01d46
Hammad Chaudhary <codewithhammad6@gmail.com>
2025-08-01 19:21:44 +0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet, StatusBar, useColorScheme } from 'react-native';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={[styles.container, { backgroundColor: isDarkMode ? '#000' : '#fff' }]}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text style={[styles.text, { color: isDarkMode ? '#fff' : '#000' }]}>
+        Hello Hammad!🚀
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
